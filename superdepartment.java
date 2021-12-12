5ae166343896f918b1daaea9af1cb46749480e2c
saurabhyadav13 <90502564+saurabhyadav13@users.noreply.github.com>
2021-12-12 23:11:52 +0530

    Create superdepartment.java

---
+++ superdepartment.java
@@ -0,0 +1,34 @@
+
+
+public class SuperDepartment {
+	
+	public String dName;
+	public String Work;
+	public String DeadLine;
+	public String holiday;
+	
+	public SuperDepartment() {
+		this.dName = " Super Department ";
+		this.Work = " No Work as of now";
+		this.DeadLine = " Nil ";
+		this.holiday = " Today is not a holiday";
+	}
+	
+	public String departmentName() {
+		return this.dName;
+	}
+	
+	public String getTodaysWork() {
+		return this.Work;
+	}
+	
+	public String getWorkDeadline() {
+		return this.DeadLine;
+	}
+	
+	public String isTodayAHoliday() {
+		return this.holiday;
+	}
+}
+
+
